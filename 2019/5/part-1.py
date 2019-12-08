d183b93f549b8eb6c84b774e71a7b2bbfc381eb1
Septatrix <24257556+Septatrix@users.noreply.github.com>
2019-12-08 18:16:21 +0100

    Add new opcodes and direct mode to day 5

--- 2019/5/part-1.py
+++ 2019/5/part-1.py
@@ -1,29 +1,47 @@
 #!/usr/bin/env python3
 
+from enum import IntEnum, unique
+
 program = input()
 parsed = list(map(int, program.split(",")))
 
-# parsed[1] = 12
-# parsed[2] = 2
+
+@unique
+class Opcode(IntEnum):
+    ADD = 1
+    MUL = 2
+    IN = 3
+    OUT = 4
+    HALT = 99
+
 
 counter = 0
-while parsed[counter] != 99:
-    modes, opcode = divmod(parsed[counter], 10)
-    if opcode == 1:
-        parsed[parsed[counter + 3]] = (
-            parsed[parsed[counter + 1]] + parsed[parsed[counter + 2]]
-        )
+while parsed[counter] % 100 != Opcode.HALT:
+    parameter_mode, opcode = divmod(parsed[counter], 100)
+    if opcode == Opcode.ADD:
+        a, b, out = parsed[counter + 1 : counter + 4]
+        if not parameter_mode & 1:
+            a = parsed[a]
+        if not parameter_mode >> 1 & 1:
+            b = parsed[b]
+        parsed[out] = a + b
         counter += 4
-    elif opcode == 2:
-        parsed[parsed[counter + 3]] = (
-            parsed[parsed[counter + 1]] * parsed[parsed[counter + 2]]
-        )
+    elif opcode == Opcode.MUL:
+        a, b, out = parsed[counter + 1 : counter + 4]
+        if not parameter_mode & 1:
+            a = parsed[a]
+        if not parameter_mode >> 1 & 1:
+            b = parsed[b]
+        parsed[out] = a * b
         counter += 4
-    elif opcode == 3:
-        parsed[parsed[counter + 1]] = int(input())
+    elif opcode == Opcode.IN:
+        parsed[parsed[counter + 1]] = int(input("Input: "))
         counter += 2
-    elif opcode == 4:
-        print(parsed[parsed[counter + 1]])
+    elif opcode == Opcode.OUT:
+        out = parsed[counter + 1]
+        if not parameter_mode & 1:
+            out = parsed[out]
+        print(f"Output: {out}")
         counter += 2
     else:
         raise ValueError("Invalid opcode")
