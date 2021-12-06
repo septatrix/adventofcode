19513c5800da92ecd5764527b17e1e9c9479303d
Septatrix <24257556+Septatrix@users.noreply.github.com>
2021-12-06 13:32:41 +0100

    Add solution for day 6

---
+++ 2021/06/src/main.rs
@@ -0,0 +1,46 @@
+use std::collections::HashMap;
+
+// TODO add deque solution
+// TODO add matrix solution (eigenvalues etc.)
+
+fn main() {
+    let fishes: Vec<u8> = include_str!("../input.txt")
+        .split(",")
+        .map(|n| n.replace("\n", "").parse().unwrap())
+        .collect();
+
+    naive(80, fishes.clone());
+    hash_map(256, fishes);
+}
+
+fn naive(days: usize, mut fishes: Vec<u8>) {
+    for _ in 0..days {
+        fishes = fishes
+            .iter()
+            .flat_map(|&f| if f == 0 { vec![6, 8] } else { vec![f - 1] })
+            .collect();
+    }
+
+    println!("#Fishes: {}", fishes.len());
+}
+
+fn hash_map(days: usize, fishes: Vec<u8>) {
+    let mut fish_map: HashMap<u8, usize> = HashMap::new();
+    for fish in fishes {
+        *fish_map.entry(fish).or_insert(0) += 1;
+    }
+
+    for _ in 0..days {
+        fish_map = fish_map.iter().fold(HashMap::new(), |mut map, (&age, &n)| {
+            if age == 0 {
+                *map.entry(6).or_insert(0) += n;
+                *map.entry(8).or_insert(0) += n;
+            } else {
+                *map.entry(age - 1).or_insert(0) += n;
+            }
+            map
+        });
+    }
+
+    println!("#Fishes: {}", fish_map.values().sum::<usize>());
+}
