cd80ed4392bc7dda21c77d37057e422174cc9f81
Septatrix <24257556+Septatrix@users.noreply.github.com>
2021-12-19 21:15:44 +0100

    Make more usage of Point struct

--- 2021/19/src/main.rs
+++ 2021/19/src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::{HashMap, HashSet};
 
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
 struct Point(i16, i16, i16);
 
 impl Point {
@@ -24,11 +25,10 @@ fn main() {
                         p.split(',').map(|n| n.parse().unwrap()).collect::<Vec<_>>(),
                     )
                     .unwrap();
-                    (a, b, c)
+                    Point(a, b, c)
                 })
                 .collect::<Vec<_>>()
         })
-        .enumerate()
         .collect::<Vec<_>>();
 
     // set first scanner as the coordinate origin
@@ -36,28 +36,27 @@ fn main() {
     scanner_pos.push(Point(0, 0, 0));
 
     let mut beacons = HashSet::new();
-    beacons.extend(scanners.pop().unwrap().1);
+    beacons.extend(scanners.pop().unwrap());
 
-    'outer: while !scanners.is_empty() {
-        for i in 0..scanners.len() {
+    while !scanners.is_empty() {
+        for (i, scanner) in scanners.clone().iter().enumerate().rev() {
             for orientation in 0..24u8 {
-                let transformed =
-                    Vec::from_iter(scanners[i].1.iter().map(|&p| rotate(p, orientation)));
+                let transformed = Vec::from_iter(scanner.iter().map(|p| rotate(p, orientation)));
                 let mut diffs = HashMap::new();
-                for (xb, yb, zb) in &transformed {
-                    for (xa, ya, za) in &beacons {
+                for Point(xb, yb, zb) in &transformed {
+                    for Point(xa, ya, za) in &beacons {
                         // increase counter for this offset
                         *diffs.entry((xa - xb, ya - yb, za - zb)).or_insert(0) += 1
                     }
                 }
                 if let Some(((dx, dy, dz), _)) = diffs.into_iter().filter(|&(_, n)| n >= 12).next()
                 {
-                    for (x, y, z) in transformed {
-                        beacons.insert((x + dx, y + dy, z + dz));
+                    for Point(x, y, z) in transformed {
+                        beacons.insert(Point(x + dx, y + dy, z + dz));
                     }
                     scanner_pos.push(Point(dx, dy, dz));
                     scanners.swap_remove(i);
-                    continue 'outer;
+                    break;
                 }
             }
         }
@@ -68,39 +67,39 @@ fn main() {
     for i in 0..scanner_pos.len() {
         for j in i..scanner_pos.len() {
             let dist = Point::distance(&scanner_pos[i], &scanner_pos[j]);
-            max_dist = std::cmp::max(max_dist, dist)
+            max_dist = max_dist.max(dist)
         }
     }
 
     println!("Covered distance: {}", max_dist);
 }
 
-fn rotate((x, y, z): (i16, i16, i16), variant: u8) -> (i16, i16, i16) {
+fn rotate(&Point(x, y, z): &Point, variant: u8) -> Point {
     match variant {
-        0 => (x, y, z),
-        1 => (x, -z, y),
-        2 => (x, -y, -z),
-        3 => (x, z, -y),
-        4 => (y, z, x),
-        5 => (y, -x, z),
-        6 => (y, -z, -x),
-        7 => (y, x, -z),
-        8 => (z, -y, x),
-        9 => (z, -x, -y),
-        10 => (z, y, -x),
-        11 => (z, x, y),
-        12 => (-x, -y, z),
-        13 => (-x, -z, -y),
-        14 => (-x, y, -z),
-        15 => (-x, z, y),
-        16 => (-y, -z, x),
-        17 => (-y, -x, -z),
-        18 => (-y, z, -x),
-        19 => (-y, x, z),
-        20 => (-z, y, x),
-        21 => (-z, -x, y),
-        22 => (-z, -y, -x),
-        23 => (-z, x, -y),
+        0 => Point(x, y, z),
+        1 => Point(x, -z, y),
+        2 => Point(x, -y, -z),
+        3 => Point(x, z, -y),
+        4 => Point(y, z, x),
+        5 => Point(y, -x, z),
+        6 => Point(y, -z, -x),
+        7 => Point(y, x, -z),
+        8 => Point(z, -y, x),
+        9 => Point(z, -x, -y),
+        10 => Point(z, y, -x),
+        11 => Point(z, x, y),
+        12 => Point(-x, -y, z),
+        13 => Point(-x, -z, -y),
+        14 => Point(-x, y, -z),
+        15 => Point(-x, z, y),
+        16 => Point(-y, -z, x),
+        17 => Point(-y, -x, -z),
+        18 => Point(-y, z, -x),
+        19 => Point(-y, x, z),
+        20 => Point(-z, y, x),
+        21 => Point(-z, -x, y),
+        22 => Point(-z, -y, -x),
+        23 => Point(-z, x, -y),
         v => panic!("Invalid variant {}", v),
     }
 }
