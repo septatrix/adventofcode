dd29054052d79834030bde6eb24d29ed94e9ef9a
Septatrix <24257556+Septatrix@users.noreply.github.com>
2021-12-26 00:01:03 +0100

    Add complete solution for day 5

--- 2021/05/src/main.rs
+++ 2021/05/src/main.rs
@@ -1,53 +1,74 @@
-const MAP_SIZE: usize = 10;
+struct LineIter {
+    curr_point: (u16, u16),
+    end: (u16, u16),
+    finished: bool,
+}
+
+impl Iterator for LineIter {
+    type Item = (u16, u16);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.curr_point == self.end {
+            if self.finished {
+                return None;
+            }
+            self.finished = true;
+            return Some(self.end);
+        }
+        let prev = self.curr_point;
+        self.curr_point = (
+            (prev.0 as i16 + (self.end.0 as i16 - prev.0 as i16).signum()) as u16,
+            (prev.1 as i16 + (self.end.1 as i16 - prev.1 as i16).signum()) as u16,
+        );
+        Some(prev)
+    }
+}
+
+const MAP_SIZE: usize = 1000;
 
 fn main() {
-    let lines = include_str!("../test.txt").lines();
+    let lines = include_str!("../input.txt").lines().map(|l| {
+        let [x1, y1, x2, y2] = <[u16; 4]>::try_from(
+            l.split(" -> ")
+                .flat_map(|p| p.split(",").map(|n| n.parse().unwrap()))
+                .collect::<Vec<_>>(),
+        )
+        .unwrap();
+        LineIter {
+            curr_point: (x1, y1),
+            end: (x2, y2),
+            finished: false,
+        }
+    });
 
-    let mut map = vec![[0u8; MAP_SIZE]; MAP_SIZE];
+    let mut map = [[0u8; MAP_SIZE]; MAP_SIZE];
 
     for line in lines {
-        let nums: Vec<u16> = line
-            .split(" -> ")
-            .flat_map(|p| {
-                p.split(",")
-                    .map(|n| n.parse().unwrap())
-                    .collect::<Vec<u16>>()
-            })
-            .collect();
-        let [x1, y1, x2, y2] = <[u16; 4]>::try_from(nums).unwrap();
-
-        // if x1 != x2 && y1 != y2 {
-        //     continue;
-        // }
-
-        let y_iter: Vec<u16> = if y1 <= y2 {
-            (y1..=y2).collect()
-        } else {
-            (y2..=y1).rev().collect()
-        };
-
-        let x_iter: Vec<u16> = if x1 <= x2 {
-            (x1..=x2).collect()
-        } else {
-            (x2..=x1).rev().collect()
-        };
-        for (y, x) in y_iter.into_iter().zip(x_iter) {
+        for (x, y) in line {
             map[y as usize][x as usize] += 1;
         }
     }
 
     let mut overlaps = 0;
 
+    println!("P2\n{} {}\n2", map.len(), map[0].len());
+
     for row in &map {
         for cell in row {
-            if cell >= &2 {
+            print!(
+                "{} ",
+                match cell {
+                    0 => 0,
+                    1 => 1,
+                    _ => 2,
+                }
+            );
+            if *cell >= 2 {
                 overlaps += 1;
             }
         }
+        println!();
     }
 
-    for row in map {
-        println!("Row: {:?}", row);
-    }
-    println!("Overlapping lines: {}", overlaps);
+    eprintln!("Overlapping lines: {}", overlaps);
 }
